e7ddb0a0b08d7239cf5761e714b781b87852f307
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-24 14:26:39 +0100

    Adds test for song created

--- tests/song_test.py
+++ tests/song_test.py
@@ -2,4 +2,10 @@ import unittest
 from classes.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.song = Song("Crystal Lake", "Disobey")
+
+    def test_has_created_song(self):
+        self.assertEqual("Crystal Lake", self.song.artist)
+        self.assertEqual("Disobey", self.song.title)
