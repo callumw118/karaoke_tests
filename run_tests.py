d263138668d67985254a413458b3af5f8fa6d5f9
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-24 14:43:38 +0100

    Adds test room has number

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from tests.guest_test import TestGuest
+from tests.guest_test import TestGuess
 from tests.room_test import TestRoom
 from tests.song_test import TestSong
 
--- tests/room_test.py
+++ tests/room_test.py
@@ -2,4 +2,9 @@ import unittest
 from classes.room import Room
 
 class TestRoom(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.room = Room(1)
+
+    def test_room_has_number(self):
+        self.assertEqual(1, self.room.room_number)
