4cd4cb0b572ec1c38fb89703dd713aec0b296c27
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-24 15:39:56 +0100

    Creates add ability to add one guest to room and tests

--- classes/room.py
+++ classes/room.py
@@ -2,10 +2,11 @@ class Room:
 
     def __init__(self, room_number):
         self.room_number = room_number
+        self.number_of_guests = []
         self.songs_queued = {}
 
     def create_room(self, room_number):
         return self.room_number
 
-    
-        
+    def add_guest_to_room(self, guests_to_add):
+        self.number_of_guests.append(guests_to_add)
--- tests/room_test.py
+++ tests/room_test.py
@@ -14,3 +14,16 @@ class TestRoom(unittest.TestCase):
 
     def test_create_room_2(self):
         self.assertEqual(2, self.room_2.create_room(self.room_2))
+
+    def test_empty_room(self):
+        self.assertEqual(0, len(self.room.number_of_guests))
+
+    def test_add_one_guest_to_room_callum(self):
+        guest_to_add = "Callum"
+        self.room.add_guest_to_room(guest_to_add)
+        self.assertEqual(["Callum"], self.room.number_of_guests)
+
+    def test_add_one_guest_to_room_james(self):
+        guest_to_add = "James"
+        self.room.add_guest_to_room(guest_to_add)
+        self.assertEqual(["James"], self.room.number_of_guests)
