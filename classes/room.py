a4ab019461c4f105c3a2dca60c70e72cba5417b7
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-24 17:01:05 +0100

    Adds comment explaining purpose of functions

--- classes/room.py
+++ classes/room.py
@@ -8,6 +8,8 @@ class Room:
     def create_room(self, room_number):
         return self.room_number
 
+    # Rather than having two functions to add one guest to the room or adding a list of guests to the room
+    # this function combines the two by first checking if there is a list of guests being passed in
     def add_guest_to_room(self, guests_to_add):
         if isinstance(guests_to_add, list):
             for guest in guests_to_add:
