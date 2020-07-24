7484245982c718148ff7465c98e0aae9bb9dd423
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-24 15:08:38 +0100

    Adds create song function and test

--- classes/song.py
+++ classes/song.py
@@ -3,3 +3,7 @@ class Song:
     def __init__(self, artist, title):
         self.artist = artist
         self.title = title
+
+    def create_song(self, song):
+        song = {self.artist: self.title}
+        return song
--- tests/song_test.py
+++ tests/song_test.py
@@ -5,6 +5,10 @@ class TestSong(unittest.TestCase):
     def setUp(self):
         self.song = Song("Crystal Lake", "Disobey")
 
-    def test_has_created_song(self):
+    def test_has_artist_and_title(self):
         self.assertEqual("Crystal Lake", self.song.artist)
         self.assertEqual("Disobey", self.song.title)
+
+    def test_created_song(self):
+        actual = self.song.create_song(self.song)
+        self.assertEqual({"Crystal Lake": "Disobey"}, actual)
